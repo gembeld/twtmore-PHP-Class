21b95a0ab64eea395c3c7b50d0f209c349fb21eb
Jamie McElwain <jamie@jgmcelwain.com>
2010-09-18 11:57:07 +0100

    Added README

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include 'twtmore.php';
-	$twtmore = new Twtmore();
-	$twtmore->apiKey = '5a5cbb6d7b4c5529a8e9974d6465eaa3';
-	echo $twtmore->tweet("jam_ie", "Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World ");
-?>
