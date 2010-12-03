259993a4d5c83c51b4ba005df51b8e152a5e6196
Jamie McElwain <jamie@jgmcelwain.com>
2010-12-03 11:26:25 -0800

    You can now longer try and post a tweet to twtmore that isn't >140 characters

--- twtmore.php
+++ twtmore.php
@@ -1,49 +1,34 @@
 <?php
-
-	class Twtmore
-	{
-
+	class Twtmore{
 		public $apiKey = false;
-
-		public function tweet($username, $content, $staging = true, $return = false)
-		{
-
-			if($this->apiKey === false)
-			{
+		public function tweet($username, $content, $staging = true, $return = false){
+			if($this->apiKey === false){
 				die('<b>Twtmore:</b> No api key set!');
 			}
-
+			if(!$content > 140){
+				die('<b>Twtmore:</b> Content must be >140 characters');
+			}
 			$curl = curl_init('http://api.twtmore.com/shorten/' . $this->apiKey);
 			curl_setopt($curl, CURLOPT_POST, 1);
-
 			$post = "user=".$username."&tweet=".$content;
-
-			if($staging)
-			{
+			if($staging){
 				$post .= "&staging=true";
 			}
-			if($return)
-			{
+			if($return){
 				$post .= "&return=url";
 			}
-
 			curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 			$response = curl_exec($curl);
 			curl_close($curl);
-
 			return $response;
-
 		}
-		public function get($code)
-		{
-
+		public function get($code){
 			$curl = curl_init('http://twtm.in/'.$code.'/content/');
 			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 			$response = curl_exec($curl);
 			curl_close($curl);
-
 			return $response;
-
 		}
 	}
+?>
